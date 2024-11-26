a376333c122728dda564293e95e070ffaaca006a
KHALID <khalidboumaghdar07@gmail.com>
2024-11-26 22:30:40 +0100

    ajout de la fonctionnalité pour créer une nouvelle tâche avec des détails (titre, description, priorité, date d'échéance)

--- pro_Sass.c
+++ pro_Sass.c
@@ -14,8 +14,66 @@
 	}tache;
 	tache ts[10];
 	int taille=0;
+void ajouter(){
+		printf("1=>Enter le titre : ");
+		scanf("%s",&ts[taille].titre);
+		printf("2=>Enter la descriprion : ");
+		scanf("%s",&ts[taille].description);
+			int choix;
+		do{
+			printf("3=>Entrer preorite (1-high 2-low) : ");
+			scanf("%d",&choix);
+			switch(choix){
+				case 1 :strcpy(ts[taille].preorite,"high");
+				break;
+				case 2 :strcpy(ts[taille].preorite,"low");
+				break;
+				default:printf("erreur!!\n");
+			}
+		}while(choix!=1 && choix!=2);
+		
+		do {
+		printf("4=>Enter le jour (doit etre > a 31): ");
+		scanf("%d",&ts[taille].date.jour);
+		if(ts[taille].date.jour<1 || ts[taille].date.jour>31){
+			printf("jour < a 31\n");
+		}
+	} while(ts[taille].date.jour<1 || ts[taille].date.jour>31);
+	do {
+		printf("5=>Enter le mois(doit etre > a 12): ");
+		scanf("%d",&ts[taille].date.mois);
+		if(ts[taille].date.mois<1 || ts[taille].date.mois>12){
+			printf("mois < a 12\n");
+		}
+	} while(ts[taille].date.mois<1 || ts[taille].date.mois>12);
+		do {
+		printf("6=>Enter l annee (doit etre > a 2024): ");
+		scanf("%d",&ts[taille].date.annee);
+		if(ts[taille].date.annee<2024){
+			printf("annee < a 2024\n");
+		}
+	} while(ts[taille].date.annee<2024);
+	
+	
+		taille++;
+		printf("Ajouter avec succes");
+	}
 int main(){
-
+int choix;
+	printf("\n \t \t\t\t\t\t\t\t !!Application de taches !! \n");
+	do {
+	
+	menu();
+	printf("=>Entrer votre choix : ");
+	scanf("%d",&choix);
+	
+	switch(choix){
+		case 1: ajouter();break;
+		case 2: printf("Au Revoir !");break;
+		default: printf("Choix invalid !");
+	}
+		
+	}while (choix!=2 );
 
 
 
