41e5fb84be1b52a1b8cb7182f7f2bfa6fc8a5a88
KHALID <khalidboumaghdar07@gmail.com>
2024-11-26 22:28:24 +0100

    Déclaration structure

---
+++ pro_Sass.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+	typedef struct{
+		int jour;
+		int mois;
+		int annee;
+	}date_d_echeance;
+	typedef struct{
+		char titre[20];
+		char description[20];
+		char preorite[20];
+		date_d_echeance date;	
+	}tache;
+	tache ts[10];
+	int taille=0;
+int main(){
+
+
+
+
+return 0;
+}
